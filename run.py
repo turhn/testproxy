fb2265c7a026aa811fdb16a75797e4ee29832f63
Turhan Coskun <turhancoskun@gmail.com>
2016-11-12 17:38:47 +0200

    More flexible proxy

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask import request
 from werkzeug.contrib.fixers import ProxyFix
+import os
 
+NUM_PROXIES = os.getenv('NUM_PROXIES', '3')
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=2)
+app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=int(NUM_PROXIES))
 
 @app.route("/")
 def hello():
     ip=request.remote_addr
-    print(ip)
-    return ip
+    return str(request.headers)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
